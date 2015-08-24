21b71e333494ae642c670860127a6d9274ddd1ee
Hana Fallisgaard <hfgaard@gmail.com>
2015-08-24 14:54:49 -0700

    finish auth template, controller and directive

--- backend/routes/login-route.js
+++ backend/routes/login-route.js
@@ -6,17 +6,17 @@ module.exports = function(router) {
   router.use(bodyParser.json());
 
   router.route('/login')
-    .post(function(req, res) {
-
-      User.findOne({username: req.body.username}, function(err, user) {
+    .get(function(req, res) {
+      var newUser = new Buffer(req.headers.authorization, 'base64');
+      var newUser = newUser.toString().split(':');
+      User.findOne({username: newUser[0]}, function(err, user) {
         if(err) return res.status(500).json({msg: 'server err'});
         if(!user) return res.status(404).json({msg: 'Username not found'});
 
-        user.checkpassword(req.body.password)
+        user.checkpassword(newUser[1])
           ? res.json({msg: 'login successful, token aquired!', token: user.generateToken()})
           : res.json({msg : 'Authentication failed'});
-
-      });
+      })
     });
 
 }
