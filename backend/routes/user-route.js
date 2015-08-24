ee71ade229c0993fcf8c111204b9310c2cac6e51
dangitsvi <dangitsvi@gmail.com>
2015-08-24 08:46:46 -0700

    add basic food route

--- backend/routes/user-route.js
+++ backend/routes/user-route.js
@@ -18,8 +18,15 @@ module.exports = function(router) {
       var user = new User(req.body);
       user.password = user.generateHash(req.body.password);
       user.save(function(err) {
-        if(err) return res.status(500).json({msg: 'Server Error: cannot save user, '  + err});
+        if(err) return res.status(500).json({msg: 'Server Error: cannot save user, \n'  + err});
         res.json({msg: 'user done been created', token:user.generateToken()});
       });
+    })
+    //delete all users
+    .delete(function(req, res) {
+      User.find({}).remove().exec(function(err) {
+        if(err) return res.status(500).json({msg: 'Server Error: cannot delete users, \n' + err});
+        res.json({msg: 'all users have been deleted. You have committed genocide. I hope you are happy'})
+      })
     });
 };
