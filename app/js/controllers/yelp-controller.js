f441c77663c5d13952499136a99b578bc36be5ed
Hana Fallisgaard <hfgaard@gmail.com>
2015-08-26 18:46:50 -0700

    food with no restaurants will post a new restaurant

--- app/js/controllers/yelp-controller.js
+++ app/js/controllers/yelp-controller.js
@@ -2,23 +2,25 @@
 
 module.exports = function(app) {
   app.controller('yelpController', ['$scope', '$http', function($scope, $http) {
-
     $scope.restaurants = [];
 
     $scope.postRestaurant = function(food, restaurant) {
-      console.log('food log \n', food.food);
-      console.log('restaurant log \n', restaurant.id);
-      console.log('comment log \n', food.comment);
       var newPost = {
         food: food.food,
         restaurant: restaurant.id,
         comment: food.comment
-      }
+      };
       $http.post('/api/foods', newPost).success(function(res) {
         $scope.restaurant.term = null;
         $scope.restaurants = null;
         $scope.food.comment = null;
-        $scope.foods.push(res.newFoodPost);
+        $scope.foodsPresent = true;
+        console.log($scope.foods);
+        if (Array.isArray($scope.foods)) {
+          $scope.foods.push(res.newFoodPost);
+        } else {
+          $scope.foods = [res.newFoodPost];
+        }
       });
     };
 
